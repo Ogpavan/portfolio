1d8a3f41f3272a79407e76f345bf017b5786b9cb
Pawan <pawantwp@gmail.com>
2024-07-27 13:47:26 +0530

    29-07-2024

--- src/pages/Home/Hero1.jsx
+++ src/pages/Home/Hero1.jsx
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import Hero2 from "./Hero2";
 import Hero3 from "./Hero3";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion"
+import sign from "../../assets/sign.png";
+import works from "../../assets/works.webp";
 
 function Hero1() {
-  const [isSignatureLoaded, setIsSignatureLoaded] = useState(false);
-  const [isWorkImageLoaded, setIsWorkImageLoaded] = useState(false);
-
   return (
     <>
-      <section className="bg-[#0F0F0F]">
+      <section className="  bg-[#0F0F0F]  ">
         <div className="max-w-[1170px] mx-auto">
-          <div className="bg-[#0F0F0F] text-white/90 flex flex-wrap gap-y-4 gap-x-4 justify-center p-6 items-center">
+          <div className="bg-[#0F0F0F] text-white/90 flex flex-wrap gap-y-4 gap-x-4  justify-center p-6 items-center">
             <motion.div
-              initial={{ opacity: 0, scale: 0.5 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0, ease: "easeInOut" }}
-              className="bg-gradient-to-br from-[#222222] to-[#101010] rounded-3xl hover:shadow-[0_10px_30px_rgba(240,_46,_170,_0.3)] duration-300 px-16 py-6 space-y-1 h-80 max-w-3xl min-w-1/2 md:w-auto flex justify-center items-center"
-            >
+            initial={{opacity:0,scale:0.5}}
+            whileInView={{opacity:1,scale:1}}
+            transition={{duration:0,ease:"easeInOut"}}
+           
+            
+            className="bg-gradient-to-br from-[#222222] to-[#101010]  rounded-3xl hover:shadow-[0_10px_30px_rgba(240,_46,_170,_0.3)] duration-300 px-16 py-6 space-y-1 h-80 max-w-3xl min-w-1/2 md:w-auto flex justify-center items-center  ">
               <div>
                 <p className="inter-tight-medium text-xl text-[#BCBCBC]">
                   A WEB DEVELOPER
                 </p>
-                <h1 className="inter-tight-bold text-6xl flex flex-col py-2">
+                <h1 className="inter-tight-bold text-6xl flex flex-col py-2 ">
                   <span>Pawan</span>
                   <span>Pal.</span>
                 </h1>
@@ -34,18 +34,17 @@ function Hero1() {
               </div>
             </motion.div>
 
-            <div className="flex flex-col gap-y-4 w-full md:w-1/2">
+            <div className="flex flex-col gap-y-4  w-full md:w-1/2 ">
               <div>
+                
                 <motion.div
-                  initial={{ opacity: 0, scale: 0.5 }}
-                  whileInView={{ opacity: 1, scale: 1 }}
-                  transition={{ duration: 0, ease: "easeInOut" }}
-                  className="bg-gradient-to-br from-[#222222] to-[#101010] rounded-3xl hover:shadow-[0_10px_30px_rgba(240,_46,_10,_0.2)] duration-300 space-y-1 cursor-pointer"
-                >
+            initial={{opacity:0,scale:0.5}}
+            whileInView={{opacity:1,scale:1}}
+            transition={{duration:0,ease:"easeInOut"}} className="bg-gradient-to-br from-[#222222] to-[#101010]  rounded-3xl hover:shadow-[0_10px_30px_rgba(240,_46,_10,_0.2)] duration-300  space-y-1 cursor-pointer ">
                   <div>
-                    <div className="text-[#9f9e9e] text-sm inter-tight-medium px-10 py-3 flex">
-                      <div className="relative overflow-hidden">
-                      <div className="flex animate-marquee whitespace-nowrap ">
+                    <div className="text-[#9f9e9e] text-sm inter-tight-medium px-10 py-3 flex  ">
+                      <div className="relative    overflow-hidden  ">
+                        <div className="flex animate-marquee whitespace-nowrap ">
                           <li className="flex px-4">
                             Latest Work and{" "}
                             <span className="text-slate-100 font-bold  px-2">
@@ -126,87 +125,89 @@ function Hero1() {
                 </motion.div>
               </div>
 
-              <div className="flex flex-col sm:flex-row justify-center gap-y-4 gap-x-4">
-                <Link to="/about">
-                  <motion.div
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    whileInView={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0, ease: "easeInOut" }}
-                    className="bg-gradient-to-br from-[#222222] to-[#101010] rounded-3xl hover:shadow-[0_10px_30px_rgba(240,_46,_10,_0.2)] duration-300 w-full sm:w-auto space-y-1"
-                  >
-                    <div className="w-full flex justify-center p-6">
-                      {!isSignatureLoaded && <div className="bg-[#222222] h-28 rounded-lg flex justify-center items-center w-full md:w-52 inter-tight-light">Loading..</div>}
+              <div className=" flex flex-col sm:flex-row justify-center gap-y-4 gap-x-4 ">
+              <Link to='/about'>
+
+              <motion.div
+            initial={{opacity:0,scale:0.5}}
+            whileInView={{opacity:1,scale:1}}
+            transition={{duration:0,ease:"easeInOut"}} className="bg-gradient-to-br from-[#222222] to-[#101010]  rounded-3xl hover:shadow-[0_10px_30px_rgba(240,_46,_10,_0.2)] duration-300  w-full sm:w-auto  space-y-1 ">
+                  <div className="w-full flex justify-center p-6">
+                    <img
+                      src={sign}
+                      alt="signature"
+                      loading="lazy"
+                    />
+                  </div>
+
+                  <div className="inter-tight-medium flex justify-between items-end p-4">
+                    <div className="flex flex-col">
+                      <p className="text-[#9f9e9e] text-sm inter-tight-medium">
+                        More About me
+                      </p>
+                      <p className="text-xl text-white/90 inter-tight-bold">
+                        Credentials
+                      </p>
+                    </div>
+                    <div className=" ">
                       <img
-                        src="https://wpriverthemes.com/landing/gridx-html/assets/images/sign.png"
-                        alt="signature"
-                        loading="lazy"
-                        onLoad={() => setIsSignatureLoaded(true)}
-                        className={isSignatureLoaded ? "block" : "hidden"}
+                        src="https://wpriverthemes.com/landing/gridx-html/assets/images/icon.svg"
+                        alt="icon"
                       />
                     </div>
-
-                    <div className="inter-tight-medium flex justify-between items-end p-4">
-                      <div className="flex flex-col">
-                        <p className="text-[#9f9e9e] text-sm inter-tight-medium">
-                          More About me
-                        </p>
-                        <p className="text-xl text-white/90 inter-tight-bold">
-                          Credentials
-                        </p>
-                      </div>
-                      <div>
-                        <img
-                          src="https://wpriverthemes.com/landing/gridx-html/assets/images/icon.svg"
-                          alt="icon"
-                        />
-                      </div>
-                    </div>
-                  </motion.div>
+                  </div>
+                </motion.div>
                 </Link>
 
-                <Link to="/work">
-                  <motion.div
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    whileInView={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0, ease: "easeInOut" }}
-                    className="bg-gradient-to-br from-[#222222] to-[#101010] rounded-3xl hover:shadow-[0_10px_30px_rgba(240,_46,_300,_0.2)] duration-300 w-full sm:w-auto space-y-1"
-                  >
-                    <div className="w-full flex justify-center p-6">
-                      {!isWorkImageLoaded && <div className="bg-[#222222] h-28 rounded-lg flex justify-center items-center md:w-52  w-full inter-tight-light">Loading..</div>}
+
+
+
+
+                <Link to='/work'>
+                <motion.div
+            initial={{opacity:0,scale:0.5}}
+            whileInView={{opacity:1,scale:1}}
+            transition={{duration:0,ease:"easeInOut"}} className="bg-gradient-to-br from-[#222222] to-[#101010]  rounded-3xl hover:shadow-[0_10px_30px_rgba(240,_46,_300,_0.2)] duration-300 w-full sm:w-auto space-y-1 ">
+                  <div className="w-full flex justify-center p-6">
+                    <img
+                      src={works}
+                      alt="work"
+                      className=""
+                    />
+                  </div>
+
+                  <div className="inter-tight-medium flex justify-between items-end p-4">
+                    <div className="flex flex-col">
+                      <p className="text-[#9f9e9e] text-sm inter-tight-medium">
+                        Showcase
+                      </p>
+                      <p className="text-xl text-white/90 inter-tight-bold">
+                        Projects
+                      </p>
+                    </div>
+                    <div className=" ">
                       <img
-                        src="https://wpriverthemes.com/landing/gridx-html/assets/images/my-works.png"
-                        alt="signature"
-                        loading="lazy"
-                        onLoad={() => setIsWorkImageLoaded(true)}
-                        className={isWorkImageLoaded ? "block" : "hidden"}
+                        src="https://wpriverthemes.com/landing/gridx-html/assets/images/icon.svg"
+                        alt="icon"
                       />
                     </div>
-
-                    <div className="inter-tight-medium flex justify-between items-end p-4">
-                      <div className="flex flex-col">
-                        <p className="text-[#9f9e9e] text-sm inter-tight-medium">
-                          Showcase
-                        </p>
-                        <p className="text-xl text-white/90 inter-tight-bold">
-                          Projects
-                        </p>
-                      </div>
-                      <div>
-                        <img
-                          src="https://wpriverthemes.com/landing/gridx-html/assets/images/icon.svg"
-                          alt="icon"
-                        />
-                      </div>
-                    </div>
-                  </motion.div>
+                  </div>
+                </motion.div>
                 </Link>
               </div>
             </div>
           </div>
+
+
+
+
+
+
+
         </div>
-        <div>
-          <Hero2 />
-          <Hero3 />
+        <div className="">
+        <Hero2 />
+        <Hero3 />
         </div>
       </section>
     </>
